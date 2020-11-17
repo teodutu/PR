10056c04763617f4c7de455729a8241c6b560b9a
teodutu <teodor.dutu@gmail.com>
2020-11-17 13:27:18 +0200

    [Lab 6] Adaugare laborator 1 de DS

---
+++ Laboratoare/Laborator6/exercitii.r
@@ -0,0 +1,49 @@
+print("==================== Ex1 ====================")
+x = rnorm(20, mean=0, sd=1)
+print(x)
+
+
+print("==================== Ex2 ====================")
+v1 = x[c(3, 5, 7, 11, 13, 19)]
+print(v1)
+
+
+print("==================== Ex3 ====================")
+v2 = x[c(-1, -2, -3)]
+print(v2)
+
+
+print("==================== Ex4 ====================")
+dice = sample(1:6, 4, TRUE)
+print(dice)
+
+
+print("==================== Ex5 ====================")
+coinFlip = sample(0:1, 100, replace = TRUE, prob = c(0.3, 0.7))
+print(coinFlip)
+print("Tails ratio:")
+print(sum(coinFlip) / length(coinFlip))
+
+
+print("==================== Ex6 ====================")
+l = list(a = 1:10, beta = exp(-3:3), logic = c(TRUE,FALSE,FALSE,TRUE))
+print("The list is:")
+print(l)
+
+print("The means of its elements are:")
+means = lapply(l, mean)
+print(means)
+
+
+print("==================== Ex7 ====================")
+A = c(1:9)
+B = c(1:12)
+C = c(1:15)
+my.lst = list(A,B,C)
+
+print("The initial list is:")
+print(my.lst)
+
+print("The list multiplied by 2 is:")
+lst.times2 = sapply(my.lst, function(x) { x * 2 })
+print(lst.times2)
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PR
 Proiectarea Retelelor - UPB 2020-2021
+
+Laboratoarele 1-5 sunt niste mizerii in *GNS3* ca la **RL**. Nu merci.
+Urmatoarele sunt de Data Science, drept care apar aici.
+
+
+## Laborator 6
+Introducere in *R* si operatii simple cu vectori, liste si `lapply`, `sapply`
+si `sample`. Simplu, dar macar nu pare un jeg ca laburile de pana acum.
